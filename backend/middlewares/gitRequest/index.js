31b10b99834b95b3743d6f53e8e695ab0c5446f7
Jose Pacheco <jpacheco@shokworks.io>
2023-09-17 12:49:51 -0400

    new api key for backend

--- backend/middlewares/gitRequest/index.js
+++ backend/middlewares/gitRequest/index.js
@@ -1,5 +1,5 @@
 const { Octokit, App } = require("octokit");
 
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 
 module.exports = octokit;
